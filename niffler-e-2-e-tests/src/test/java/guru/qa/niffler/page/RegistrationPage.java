48a9cdfec5c2e8eb12cb84bdcd13b55be035a233
avadanilov <avadanilov@yandex.ru>
2024-09-13 09:57:00 +0300

    [NF] add RegistrationPage object

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/RegistrationPage.java
@@ -0,0 +1,20 @@
+package guru.qa.niffler.page;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class RegistrationPage {
+  private final SelenideElement usernameInput = $("#username");
+  private final SelenideElement passwordInput = $("#password");
+  private final SelenideElement submitPasswordInput = $("#passwordSubmit");
+  private final SelenideElement submitButton = $("button[type='submit']");
+
+  public MainPage login(String username, String password) {
+    usernameInput.setValue(username);
+    passwordInput.setValue(password);
+    submitPasswordInput.setValue(password);
+    submitButton.click();
+    return new MainPage();
+  }
+}
