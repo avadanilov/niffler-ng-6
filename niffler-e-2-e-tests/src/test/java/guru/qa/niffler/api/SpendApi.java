b02aa41e38991e3742bf5c75290206fa6ac4f95f
avadanilov <avadanilov@yandex.ru>
2024-09-15 12:00:49 +0300

    [NF] add api in SpendApi.java

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
@@ -2,11 +2,24 @@ package guru.qa.niffler.api;
 
 import guru.qa.niffler.model.SpendJson;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
+import retrofit2.http.*;
 
 public interface SpendApi {
 
   @POST("internal/spends/add")
   Call<SpendJson> addSpend(@Body SpendJson spend);
+  @PATCH("internal/spends/edit")
+  Call<SpendJson> editSpend(@Body SpendJson spend);
+  @GET("internal/spends/{id}]")
+  Call<SpendJson> getSpend(@Body SpendJson spend);
+  @GET("internal/spends/all]")
+  Call<SpendJson> getAllSpends(@Body SpendJson spend);
+  @DELETE("internal/spends/remove")
+  Call<SpendJson> deleteSpend(@Body SpendJson spend);
+  @POST("internal/categories/add")
+  Call<SpendJson> addCategory(@Body SpendJson spend);
+  @PATCH("internal/categories/update")
+  Call<SpendJson> updateCategory(@Body SpendJson spend);
+  @GET("internal/categories/all")
+  Call<SpendJson> getAllCategories(@Body SpendJson spend);
 }
