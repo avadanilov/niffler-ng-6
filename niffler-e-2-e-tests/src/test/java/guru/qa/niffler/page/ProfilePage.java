163c2778d31df0060deae51176797edc5519ae74
avadanilov <avadanilov@yandex.ru>
2024-09-15 10:23:59 +0300

    [NF] add ProfilePage object

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/ProfilePage.java
@@ -0,0 +1,66 @@
+package guru.qa.niffler.page;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class ProfilePage {
+    private final SelenideElement uploadNewPictureButton = $(".image__input-label");
+    private final SelenideElement usernameInput = $("#username");
+    private final SelenideElement nameInput = $("#name");
+    private final SelenideElement saveChangesButton = $(withText("Save changes"));
+    private final SelenideElement showArchivedCheckbox = $(".PrivateSwitchBase-input");
+    private final SelenideElement addNewCategoryInput = $("#category");
+    private final SelenideElement archiveConfirmationDialog = $(".MuiDialog-container");
+    private final SelenideElement closeButton = archiveConfirmationDialog.$(withText("Close"));
+    private final SelenideElement archiveButton = archiveConfirmationDialog.$(withText("Archive"));
+    private final ElementsCollection categories = addNewCategoryInput.parent().$$(":scope ~ div");
+
+    private final String categoryChipLocator = ".MuiChip-labelMedium";
+
+    private static void editCategoryInputField(String newCategoryName, SelenideElement category) {
+        category.$("#category")
+                .setValue(newCategoryName)
+                .unfocus();
+        category.$("button").click();
+    }
+
+    private SelenideElement getCategory(String categoryName) {
+        categories.shouldHave(sizeGreaterThan(0));
+        return categories.stream().filter(
+                        category -> category.$(categoryChipLocator).has(text(categoryName))
+                )
+                .findFirst()
+                .orElseThrow();
+    }
+
+    public ProfilePage editCategory(String categoryName, String newCategoryName) {
+        SelenideElement category = getCategory(categoryName);
+        category.$(categoryChipLocator).click();
+        editCategoryInputField(newCategoryName, category);
+        return this;
+    }
+
+    public ProfilePage editCategoryByClickEditButton(String categoryName, String newCategoryName) {
+        SelenideElement category = getCategory(categoryName);
+        category.$("[aria-label='Edit category']")
+                .click();
+        editCategoryInputField(newCategoryName, category);
+        return this;
+    }
+
+    public ProfilePage archiveCategory(String categoryName) {
+        SelenideElement category = getCategory(categoryName);
+        category.$("[aria-label='Archive category']")
+                .click();
+        archiveConfirmationDialog.should(visible);
+        archiveButton.click();
+        return this;
+    }
+
+}
