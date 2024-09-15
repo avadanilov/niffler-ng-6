7d757a19e71e3c63b17f3ae0b20226e1359bbe43
avadanilov <avadanilov@yandex.ru>
2024-09-15 10:25:43 +0300

    [RF] fix RegisterPage

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/RegisterPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/RegisterPage.java
@@ -4,17 +4,17 @@ import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$;
 
-public class RegistrationPage {
+public class RegisterPage {
   private final SelenideElement usernameInput = $("#username");
   private final SelenideElement passwordInput = $("#password");
   private final SelenideElement submitPasswordInput = $("#passwordSubmit");
   private final SelenideElement submitButton = $("button[type='submit']");
 
-  public MainPage signUp(String username, String password) {
+  public LoginPage signUp(String username, String password) {
     usernameInput.setValue(username);
     passwordInput.setValue(password);
     submitPasswordInput.setValue(password);
     submitButton.click();
-    return new MainPage();
+    return new LoginPage();
   }
 }
