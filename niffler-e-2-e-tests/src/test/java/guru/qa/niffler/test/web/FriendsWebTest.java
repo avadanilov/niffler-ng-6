6866da3b433350f3ceae17b962e54e3d4c79c10d
avadanilov <avadanilov@yandex.ru>
2024-10-06 18:28:04 +0300

    [RF] refactor tests

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FriendsWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FriendsWebTest.java
@@ -6,14 +6,15 @@ import guru.qa.niffler.jupiter.extension.UsersQueueExtension;
 import guru.qa.niffler.page.AllPeoplePage;
 import guru.qa.niffler.page.FriendsPage;
 import guru.qa.niffler.page.LoginPage;
-import guru.qa.niffler.page.MainPage;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import static com.codeborne.selenide.CollectionCondition.*;
+import static com.codeborne.selenide.CollectionCondition.empty;
+import static com.codeborne.selenide.CollectionCondition.itemWithText;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
-import static guru.qa.niffler.jupiter.extension.UsersQueueExtension.*;
+import static com.codeborne.selenide.Selenide.open;
+import static guru.qa.niffler.jupiter.extension.UsersQueueExtension.StaticUser;
+import static guru.qa.niffler.jupiter.extension.UsersQueueExtension.UserType;
 import static guru.qa.niffler.jupiter.extension.UsersQueueExtension.UserType.Type.*;
 
 @ExtendWith(BrowserExtension.class)
@@ -23,10 +24,9 @@ public class FriendsWebTest {
     @Test
     @ExtendWith(UsersQueueExtension.class)
     void friendShouldBePresentInFriendsTable(@UserType(WITH_FRIEND) StaticUser user) {
-        MainPage mainPage =
-                open(CFG.frontUrl(), LoginPage.class)
-                        .login(user.username(), user.password());
-        mainPage.checkPageLoaded();
+        open(CFG.frontUrl(), LoginPage.class)
+                .login(user.username(), user.password())
+                .checkPageLoaded();
         FriendsPage friendsPage =
                 open(CFG.frontUrl() + "people/friends", FriendsPage.class);
         friendsPage.getFriendsNames()
@@ -37,10 +37,9 @@ public class FriendsWebTest {
     @Test
     @ExtendWith(UsersQueueExtension.class)
     void friendsTableShouldBeEmptyForNewUser(@UserType(EMPTY) StaticUser user) {
-        MainPage mainPage =
-                open(CFG.frontUrl(), LoginPage.class)
-                        .login(user.username(), user.password());
-        mainPage.checkPageLoaded();
+        open(CFG.frontUrl(), LoginPage.class)
+                .login(user.username(), user.password())
+                .checkPageLoaded();
         FriendsPage friendsPage =
                 open(CFG.frontUrl() + "people/friends", FriendsPage.class);
         friendsPage.getFriendsNames()
@@ -53,10 +52,9 @@ public class FriendsWebTest {
     @Test
     @ExtendWith(UsersQueueExtension.class)
     void incomeInvitationBePresentInFriendsTable(@UserType(WITH_INCOME_REQUEST) StaticUser user) {
-        MainPage mainPage =
-                open(CFG.frontUrl(), LoginPage.class)
-                        .login(user.username(), user.password());
-        mainPage.checkPageLoaded();
+        open(CFG.frontUrl(), LoginPage.class)
+                .login(user.username(), user.password())
+                .checkPageLoaded();
         FriendsPage friendsPage =
                 open(CFG.frontUrl() + "people/friends", FriendsPage.class);
         friendsPage.getRequestNames()
@@ -66,10 +64,9 @@ public class FriendsWebTest {
     @Test
     @ExtendWith(UsersQueueExtension.class)
     void outcomeInvitationBePresentInAllPeoplesTable(@UserType(WITH_OUTCOME_REQUEST) StaticUser user) {
-        MainPage mainPage =
-                open(CFG.frontUrl(), LoginPage.class)
-                        .login(user.username(), user.password());
-        mainPage.checkPageLoaded();
+        open(CFG.frontUrl(), LoginPage.class)
+                .login(user.username(), user.password())
+                .checkPageLoaded();
         AllPeoplePage allPeoplePage =
                 open(CFG.frontUrl() + "people/all", AllPeoplePage.class);
         allPeoplePage.getTableItemWithName(user.outcome())
