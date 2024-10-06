ecaac420768c304c22a756a2acc3c602e034c8eb
avadanilov <avadanilov@yandex.ru>
2024-10-06 18:06:29 +0300

    [NF] add friendsTableShouldBeEmptyForNewUser test

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/FriendsPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/FriendsPage.java
@@ -1,6 +1,7 @@
 package guru.qa.niffler.page;
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import lombok.Getter;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -8,5 +9,6 @@ import static com.codeborne.selenide.Selenide.$;
 @Getter
 public class FriendsPage {
     private final ElementsCollection friendsNames = $("#friends").$$("div .MuiBox-root");
+    private final SelenideElement emptyListPanel = $("#simple-tabpanel-friends");
 
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FriendsWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/FriendsWebTest.java
@@ -9,7 +9,8 @@ import guru.qa.niffler.page.MainPage;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.CollectionCondition.itemWithText;
+import static com.codeborne.selenide.CollectionCondition.*;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static guru.qa.niffler.jupiter.extension.UsersQueueExtension.*;
 import static guru.qa.niffler.jupiter.extension.UsersQueueExtension.UserType.Type.*;
@@ -33,7 +34,18 @@ public class FriendsWebTest {
 
     @Test
     @ExtendWith(UsersQueueExtension.class)
-    void friendsTableShouldBeEmptyForNewUser(@UserType(EMPTY) StaticUser user) {}
+    void friendsTableShouldBeEmptyForNewUser(@UserType(EMPTY) StaticUser user) {
+        MainPage mainPage =
+                open(CFG.frontUrl(), LoginPage.class)
+                        .login(user.username(), user.password());
+        mainPage.checkPageLoaded();
+        FriendsPage friendsPage =
+                open(CFG.frontUrl() + "people/friends", FriendsPage.class);
+        friendsPage.getFriendsNames().should(empty);
+        friendsPage.getEmptyListPanel().should(text("There are no users yet"));
+    }
+
+
     @Test
     @ExtendWith(UsersQueueExtension.class)
     void incomeInvitationBePresentInFriendsTable(@UserType(WITH_INCOME_REQUEST) StaticUser user) {}
